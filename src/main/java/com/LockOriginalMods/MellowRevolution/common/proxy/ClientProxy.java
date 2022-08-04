df50be18a1e4af63cdfcda9cfa97e0c16fe028f7
LockOriginalMods <81614998+LockOriginalMods@users.noreply.github.com>
2022-08-05 00:44:21 +0300

    Delete src/main directory

--- src/main/java/com/LockOriginalMods/MellowRevolution/common/proxy/ClientProxy.java
+++
@@ -1,9 +0,0 @@
-package com.LockOriginalMods.MellowRevolution.common.proxy;
-
-public class ClientProxy extends CommonProxy {
-
-    @Override
-    public void start() {
-        super.start();
-    }
-}
